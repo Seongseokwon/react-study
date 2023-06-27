f58375be321d1fe77983fbeab288759874eb8490
Seongseokwon <sseong@deepnoid.com>
2023-06-27 14:00:39 +0900

    App useEffect 제거

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,15 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Login from './pages/auth/Login';
 import Signup from "./pages/auth/Signup";
-import _Todo from "./pages/_Todo";
+import Todo from "./pages/_Todo";
 
 function App() {
-
-    useEffect(() => {
-        fetch('/todos')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-        }) 
-    })
     return (
         <Routes>
             <Route path='/' element={<Main />} />
             <Route path='/signin' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
-            <Route path='/todo' element={<_Todo />} />
+            <Route path='/todo' element={<Todo />} />
         </Routes>
     );
 }
