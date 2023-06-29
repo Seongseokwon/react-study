d9a294ce3050972ff14e1e3c3254cfec9b0531e4
Seongseokwon <sseong@deepnoid.com>
2023-06-29 17:23:04 +0900

    input height 수정

--- src/components/styles/Common.styled.js
+++ src/components/styles/Common.styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import MainImg from '../../assets/img/main_img_2048.png';
+
 export const StyledCommonLayout = styled.div`
   -webkit-border-radius: 10px;
   -moz-border-radius: 10px;
@@ -17,19 +18,28 @@ export const StyledCommonLayout = styled.div`
 `
 
 export const StyledCommonInputGroup = styled.div`
-  
-  
+
+
   display: flex;
-  
+  padding: 10px;
+
   & > label {
-    width: 150px;
+    width: 120px;
+    font-size: 18px;
+    font-weight: bold;
   }
-  
+
   & > input {
     width: 350px;
+    padding: 5px;
+    height: 35px;
+    border: 1px solid lightgrey;
+    box-shadow: 8px 8px 14px -2px rgba(0,0,0,0.75);
+    -webkit-box-shadow: 8px 8px 14px -2px rgba(0,0,0,0.75);
+    -moz-box-shadow: 8px 8px 14px -2px rgba(0,0,0,0.75);
   }
-  
-  
+
+
 `
 
 
